3502ece99a361d5910a0e68e95c153df4f1f2f78
uwx <cadrekucra@gmx.com>
2020-08-03 22:51:33 -0300

    Fix runtime error due to property referencing itself in object initializer

--- GTASDK.Generator/TypeCache.cs
+++ GTASDK.Generator/TypeCache.cs
@@ -6,117 +6,141 @@ namespace GTASDK.Generator
 {
     public static class Types
     {
-        public static readonly IReadOnlyDictionary<string, BuiltinType> Builtin = new Dictionary<string, BuiltinType>
+        private static class PresetTypes
         {
-            ["<pointer>"] = new BuiltinType
-            {
-                Size = 0x4,
-                Template = new GetSetTemplate("(IntPtr)({0})", "Memory.WriteUInt32({0}, (int)value)")
-            },
-            // Size 1
-            ["<byte>"] = new BuiltinType
+            public static readonly BuiltinType Byte = new BuiltinType
             {
                 TypeMapsTo = "byte",
                 Size = sizeof(byte),
                 Template = new GetSetTemplate("Memory.ReadByte({0})", "Memory.WriteByte({0}, value)"),
                 BitsTemplate = new GetSetTemplate(@"Memory.ReadBitsInt8({0}, {1}, {2})", @"throw new InvalidOperationException(""NOT DONE YET"")")
-            },
-            ["<sbyte>"] = new BuiltinType
+            };
+
+            public static readonly BuiltinType SByte = new BuiltinType
             {
                 TypeMapsTo = "sbyte",
                 Size = sizeof(sbyte),
                 Template = new GetSetTemplate("Memory.ReadSByte({0})", "Memory.WriteSByte({0}, value)")
-            },
+            };
 
-            ["bool"] = Builtin["<byte>"],
-            ["char"] = Builtin["<byte>"],
-            ["char8_t"] = Builtin["<byte>"],
-            ["__int8"] = Builtin["<byte>"],
-            ["unsigned char"] = Builtin["<byte>"],
-            ["signed char"] = Builtin["<sbyte>"],
-
-            // Size 2
-            ["<short>"] = new BuiltinType
+            public static readonly BuiltinType Short = new BuiltinType
             {
                 TypeMapsTo = "short",
                 Size = sizeof(short),
                 Template = new GetSetTemplate("Memory.ReadInt16({0})", "Memory.WriteInt16({0}, value)")
-            },
-            ["<ushort>"] = new BuiltinType
+            };
+
+            public static readonly BuiltinType UShort = new BuiltinType
             {
                 TypeMapsTo = "ushort",
                 Size = sizeof(ushort),
                 Template = new GetSetTemplate("Memory.ReadUInt16({0})", "Memory.WriteUInt16({0}, value)")
-            },
+            };
 
-            ["short"] = Builtin["<short>"],
-            ["__int16"] = Builtin["<short>"],
-            ["short int"] = Builtin["<short>"],
-            ["unsigned short"] = Builtin["<ushort>"],
-            ["unsigned __int16"] = Builtin["<ushort>"],
-            ["unsigned short int"] = Builtin["<ushort>"],
-
-            // Size 4
-            ["<int>"] = new BuiltinType
+            public static readonly BuiltinType Int = new BuiltinType
             {
                 TypeMapsTo = "int",
                 Size = sizeof(int),
                 Template = new GetSetTemplate("Memory.ReadInt32({0})", "Memory.WriteInt32({0}, value)")
-            },
-            ["<uint>"] = new BuiltinType
+            };
+            
+            public static readonly BuiltinType UInt = new BuiltinType
             {
                 TypeMapsTo = "uint",
                 Size = sizeof(uint),
                 Template = new GetSetTemplate("Memory.ReadUInt32({0})", "Memory.WriteUInt32({0}, value)")
-            },
+            };
 
-            ["__int32"] = Builtin["<int>"],
-            ["signed"] = Builtin["<int>"],
-            ["signed int"] = Builtin["<int>"],
-            ["int"] = Builtin["<int>"],
-            ["long"] = Builtin["<int>"],
-            ["long int"] = Builtin["<int>"],
-            ["signed long int"] = Builtin["<int>"],
-            ["unsigned __int32"] = Builtin["<uint>"],
-            ["unsigned"] = Builtin["<uint>"],
-            ["unsigned int"] = Builtin["<uint>"],
-            ["unsigned long"] = Builtin["<uint>"],
-            ["unsigned long int"] = Builtin["<uint>"],
-
-            // Size 8
-            ["<long>"] = new BuiltinType
+            public static readonly BuiltinType Long = new BuiltinType
             {
                 TypeMapsTo = "long",
                 Size = sizeof(long),
                 Template = new GetSetTemplate("Memory.ReadInt64({0})", "Memory.WriteInt64({0}, value)")
-            },
-            ["<ulong>"] = new BuiltinType
+            };
+
+            public static readonly BuiltinType ULong = new BuiltinType
             {
                 TypeMapsTo = "ulong",
                 Size = sizeof(ulong),
                 Template = new GetSetTemplate("Memory.ReadUInt64({0})", "Memory.WriteUInt64({0}, value)")
-            },
+            };
 
-            ["__int64"] = Builtin["<long>"],
-            ["long long"] = Builtin["<long>"],
-            ["signed long long"] = Builtin["<long>"],
-            ["unsigned __int64"] = Builtin["<ulong>"],
-            ["unsigned long long"] = Builtin["<ulong>"],
-
-            // Floating-point
-            ["float"] = new BuiltinType
+            public static readonly BuiltinType Float = new BuiltinType
             {
                 TypeMapsTo = "float",
                 Size = sizeof(float),
                 Template = new GetSetTemplate("Memory.ReadFloat({0})", "Memory.WriteFloat({0}, value)")
-            },
-            ["double"] = new BuiltinType
+            };
+
+            public static readonly BuiltinType Double = new BuiltinType
             {
                 TypeMapsTo = "double",
                 Size = sizeof(double),
                 Template = new GetSetTemplate("Memory.ReadDouble({0})", "Memory.WriteDouble({0}, value)")
-            },
-            ["long double"] = Builtin["double"],
+            };
+        }
+
+        public static BuiltinType Pointer { get; } = new BuiltinType
+        {
+            Size = 0x4,
+            Template = new GetSetTemplate("(IntPtr)({0})", "Memory.WriteUInt32({0}, (int)value)")
+        };
+
+        public static readonly IReadOnlyDictionary<string, BuiltinType> Builtin = new Dictionary<string, BuiltinType>
+        {
+            // Size 1
+            ["<byte>"] = PresetTypes.Byte,
+            ["<sbyte>"] = PresetTypes.SByte,
+
+            ["bool"] = PresetTypes.Byte,
+            ["char"] = PresetTypes.Byte,
+            ["char8_t"] = PresetTypes.Byte,
+            ["__int8"] = PresetTypes.Byte,
+            ["unsigned char"] = PresetTypes.Byte,
+            ["signed char"] = PresetTypes.SByte,
+
+            // Size 2
+            ["<short>"] = PresetTypes.Short,
+            ["<ushort>"] = PresetTypes.UShort,
+
+            ["short"] = PresetTypes.Short,
+            ["__int16"] = PresetTypes.Short,
+            ["short int"] = PresetTypes.Short,
+            ["unsigned short"] = PresetTypes.UShort,
+            ["unsigned __int16"] = PresetTypes.UShort,
+            ["unsigned short int"] = PresetTypes.UShort,
+
+            // Size 4
+            ["<int>"] = PresetTypes.Int,
+            ["<uint>"] = PresetTypes.UInt,
+
+            ["__int32"] = PresetTypes.Int,
+            ["signed"] = PresetTypes.Int,
+            ["signed int"] = PresetTypes.Int,
+            ["int"] = PresetTypes.Int,
+            ["long"] = PresetTypes.Int,
+            ["long int"] = PresetTypes.Int,
+            ["signed long int"] = PresetTypes.Int,
+            ["unsigned __int32"] = PresetTypes.UInt,
+            ["unsigned"] = PresetTypes.UInt,
+            ["unsigned int"] = PresetTypes.UInt,
+            ["unsigned long"] = PresetTypes.UInt,
+            ["unsigned long int"] = PresetTypes.UInt,
+
+            // Size 8
+            ["<long>"] = PresetTypes.Long,
+            ["<ulong>"] = PresetTypes.ULong,
+
+            ["__int64"] = PresetTypes.Long,
+            ["long long"] = PresetTypes.Long,
+            ["signed long long"] = PresetTypes.Long,
+            ["unsigned __int64"] = PresetTypes.ULong,
+            ["unsigned long long"] = PresetTypes.ULong,
+
+            // Floating-point
+            ["float"] = PresetTypes.Float,
+            ["double"] = PresetTypes.Double,
+            ["long double"] = PresetTypes.Double,
 
             // Special/placeholder
             ["CPlaceable"] = new BuiltinType
@@ -125,8 +149,6 @@ namespace GTASDK.Generator
                 Template = new GetSetTemplate("new CPlaceable({0})", @"throw new InvalidOperationException(""NOT DONE YET"")")
             }
         };
-
-        public static BuiltinType Pointer => Builtin["<pointer>"];
     }
 
     public abstract class ParserType
